7cebb6e295e4deeda62c13b4743adb53bf5fa884
Yuri Bukatkin <yurij-bukatkin@yandex.ru>
2017-07-18 02:49:53 +0300

    Update test_pyrabbit.py

--- tests/test_pyrabbit.py
+++ tests/test_pyrabbit.py
@@ -17,7 +17,7 @@ from mock import Mock, patch
 
 class TestClient(unittest.TestCase):
     def setUp(self):
-        self.client = pyrabbit.api.Client('localhost:55672/api', 'guest', 'guest')
+        self.client = pyrabbit2.api.Client('localhost:55672/api', 'guest', 'guest')
 
     def tearDown(self):
         del self.client
@@ -199,7 +199,7 @@ class TestClient(unittest.TestCase):
 @unittest.skip
 class TestLiveServer(unittest.TestCase):
     def setUp(self):
-        self.rabbit = pyrabbit.api.Client('localhost:15672', 'guest', 'guest')
+        self.rabbit = pyrabbit2.api.Client('localhost:15672', 'guest', 'guest')
         self.vhost_name = 'pyrabbit_test_vhost'
         self.exchange_name = 'pyrabbit_test_exchange'
         self.queue_name = 'pyrabbit_test_queue'
